fe497a9075d7e6e3568b25ac32c3dd6b29707c28
Mickey L540 <ntwari83@gmail.com>
2023-09-01 12:39:52 +0200

    first commit

---
+++ src/main/java/com/blive/email_Schdl/EmailSchdlApplication.java
@@ -0,0 +1,13 @@
+package com.blive.email_Schdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSchdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSchdlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blive/email_Schdl/EmailSchdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blive.email_Schdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSchdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
